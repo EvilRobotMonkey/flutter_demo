ab994d5e2f2735125576391bf9f6dc189bddb139
Tao_oaT <tao0312tao@gmail.com>
2020-06-04 14:53:10 +0800

    底部弹出框实现

--- lib/store/page/store_audit_page.dart
+++ lib/store/page/store_audit_page.dart
@@ -13,6 +13,7 @@ import 'package:flutterapp/widgets/app_bar.dart';
 import 'package:flutterapp/widgets/my_button.dart';
 import 'package:flutterapp/widgets/my_scroll_view.dart';
 import 'package:flutterapp/widgets/selected_image.dart';
+import 'package:flutterapp/widgets/store_select_text_item.dart';
 import 'package:flutterapp/widgets/text_field_item.dart';
 import 'package:image_picker/image_picker.dart';
 import 'package:keyboard_actions/keyboard_action.dart';
@@ -113,7 +114,84 @@ class _StoreAuditPageState extends State<StoreAuditPage> {
                 .copyWith(fontSize: Dimens.font_sp14)),
       ),
       Gaps.vGap16,
-      TextFieldItem(focusNode: _nodeText1, title: '店铺名称', hintText: '填写店铺名称')
+      TextFieldItem(focusNode: _nodeText1, title: '店铺名称', hintText: '填写店铺名称'),
+      StoreSelectTextItem(
+          title: '主营范围', content: _sortName, onTap: () => _showBottomSheet()),
+      StoreSelectTextItem(
+        title: '店铺地址',
+        content: _address,
+        onTap: () {
+//          NavigatorUtils.pushResult(context, ShopRouter.addressSelectPage, (result) {
+//            setState(() {
+//              PoiSearch model = result;
+//              _address = model.provinceName + ' ' +
+//                  model.cityName + ' ' +
+//                  model.adName + ' ' +
+//                  model.title;
+//            });
+//          });
+        },
+      ),
+      Gaps.vGap32,
+      const Padding(
+        padding: const EdgeInsets.only(left: 16.0),
+        child: const Text('店主信息', style: TextStyles.textBold18),
+      ),
+      Gaps.vGap16,
+      TextFieldItem(focusNode: _nodeText2, title: '店主姓名', hintText: '填写店主姓名'),
+      TextFieldItem(
+          focusNode: _nodeText3,
+          keyboardType: TextInputType.phone,
+          title: '联系电话',
+          hintText: '填写店主联系电话')
     ];
+  }
+
+  String _sortName = '';
+  var _list = [
+    '水果生鲜',
+    '家用电器',
+    '休闲食品',
+    '茶酒饮料',
+    '美妆个护',
+    '粮油调味',
+    '家庭清洁',
+    '厨具用品',
+    '儿童玩具',
+    '床上用品'
+  ];
+
+  _showBottomSheet() {
+    showModalBottomSheet(
+        context: context,
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.only(
+              topLeft: Radius.circular(10), topRight: Radius.circular(10)),
+        ),
+        builder: (BuildContext context) {
+          return SizedBox(
+            height: 360.0,
+            child: ListView.builder(
+              physics: BouncingScrollPhysics(),
+              itemBuilder: (_, index) {
+                return InkWell(
+                  child: Container(
+                    padding: EdgeInsets.only(left: 16.0),
+                    alignment: Alignment.centerLeft,
+                    child: Text(_list[index]),
+                  ),
+                  onTap: () {
+                    setState(() {
+                      _sortName = _list[index];
+                    });
+                    NavigatorUtils.goBack(context);
+                  },
+                );
+              },
+              itemExtent: 48.0,
+              itemCount: _list.length,
+            ),
+          );
+        });
   }
 }
